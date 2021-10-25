80c6a80df7c5473500915179071e06558c3cbe58
IvanWeissPol <68245044+IvanWeissPol@users.noreply.github.com>
2021-10-25 10:46:32 -0300

    added coments

--- program/test_cases_generator/test_cases.py
+++ program/test_cases_generator/test_cases.py
@@ -9,10 +9,12 @@ def add_title(df ,title):
     row_df = pd.DataFrame([row])
     df = pd.concat([df,row_df],ignore_index=True)
     return df
+
 def add_subtitle(df ,subtitle):
     row = pd.Series(subtitle)
     row_df = pd.DataFrame([row])
     df = pd.concat([df,row_df],ignore_index=True)
+    #row bellow every subtitle to understand what each column is
     row = pd.Series(["#","Element","Case","Test message","Status","Error","Observations","optional","BaseType","restricted values","Enumerations","tested values"])
     row_df = pd.DataFrame([row])
     df = pd.concat([df,row_df],ignore_index=True)
@@ -21,9 +23,12 @@ def add_subtitle(df ,subtitle):
 def add_test_cases(df ,row,columns,title,cont):
     list_of_cases = row.tests
     list_of_cases = list_of_cases.split(" ")
+
+    #read the name of the element
     for string in columns:
         if string.startswith('L') and not pd.isnull(row[string]):
             element = row[string]
+    # add all other info from row
     optional = "false"
     if(row.Cardinality =="0..1"):
         optional = "true"
@@ -33,10 +38,12 @@ def add_test_cases(df ,row,columns,title,cont):
     BaseType = row['BaseType']
     Enumerations = row["Enumerations"]
     restricted_values = row.Constraints
+    #make a test case for each case
     for case in list_of_cases:
         test_message = title + "_" + str(cont)
         if case == 'make2WithSameID':
             test_message = test_message+"_1 \n" + test_message+"_2"
+        
         new_row = [cont,element,case,test_message,status,error,observation,optional,BaseType,restricted_values,Enumerations]
         row_df = pd.DataFrame([new_row])
         df = pd.concat([df,row_df],ignore_index=True)
@@ -49,11 +56,13 @@ def make_test_cases(message_details_folder_path):
         path = message_details_folder_path + "\\" + excel
         df = pd.read_excel(path)
         
+        #remove the columns that are not needed
         df = df[df.columns.difference(['Type'])]
-        #df = df[df.columns.difference(['BaseType'])]
+
         excel_df = pd.DataFrame()
         columns=list(df.columns.values)
         test_cases_cont = 0
+        #read the rows
         for row in df.iterrows():
             row = row[1]
             if not(pd.isnull(row.L1)) or not(pd.isnull(row.L2)):
@@ -65,6 +74,7 @@ def make_test_cases(message_details_folder_path):
             else:
                 if not(pd.isnull(row.tests)):
                     test_cases_cont ,excel_df = add_test_cases(df=excel_df, row=row, columns=columns, title=title, cont=test_cases_cont)
+        #save the excel
         path = paths.test_cases_folder_path + "\\test_cases_" + title + ".xlsx"
         writer = pd.ExcelWriter(path=path, engine='xlsxwriter')
         excel_df.to_excel(writer, sheet_name='Sheet1',index=False)
